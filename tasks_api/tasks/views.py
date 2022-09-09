ddf6ab30152e49755d3a455655df9c0b23f67878
Cenk Arda Yılmaz <e2310670@ceng.metu.edu.tr>
2022-09-09 16:13:34 +0300

    create TaskViewSet as a View for tasks

--- tasks_api/tasks/views.py
+++ tasks_api/tasks/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Task
+from .serializers import TaskSerializer
 
-# Create your views here.
+
+class TaskViewSet(viewsets.ModelViewSet):
+    queryset = Task.objects.all()
+    serializer_class = TaskSerializer
+    permission_classes = []  # authentication is not required for this task
